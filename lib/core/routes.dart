59127a1467bc734d547c726be23ca9cf778e9ae9
Your Name <you@example.com>
2023-08-03 11:35:22 +0300

    add routes images

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,5 +1,5 @@
 class Routes {
   static const String splachScreen = '/splach_screen';
-  static const String aboutScreen = '/main_screen';
+  static const String outBoarding = '/out_boarding';
   static const String mainScreen = '/main_screen';
 }
