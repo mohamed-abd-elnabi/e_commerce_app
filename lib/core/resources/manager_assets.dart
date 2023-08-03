0cfc7e4a341b48b887e96e7171d8891e449a3977
Your Name <you@example.com>
2023-08-03 11:35:52 +0300

    add assets and constants

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -1,3 +1,6 @@
 class ManagerAssets {
   static const String azkarImage = 'assets/images/azkar.jpg';
+  static const String splash1 = 'assets/images/splach/splach1.png';
+  static const String splash2 = 'assets/images/splach/splach2.png';
+  static const String splash3 = 'assets/images/splach/splach3.png';
 }
