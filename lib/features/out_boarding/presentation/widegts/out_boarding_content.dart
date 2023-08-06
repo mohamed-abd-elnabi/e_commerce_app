1a4835bd572257ace0952b18bfc3759fd7d745d0
Your Name <you@example.com>
2023-08-06 03:43:24 +0300

    create outboardingContant

---
+++ lib/features/out_boarding/presentation/widegts/out_boarding_content.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/constants.dart';
+import '../../../../core/resources/managar_font_size.dart';
+import '../../../../core/resources/manager_assets.dart';
+import '../../../../core/resources/manager_colors.dart';
+import '../../../../core/resources/manager_font_weight.dart';
+import '../../../../core/resources/manager_height.dart';
+import '../../../../core/resources/manager_strings.dart';
+
+Column outBoardingContent({
+  String image = ManagerAssets.outboarding1,
+  String title = ManagerStrings.outboardingTitleOne,
+  String subtitle = ManagerStrings.subtitle1,
+}) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      Image.asset(image),
+      Text(
+        title,
+        style: TextStyle(
+            color: ManagerColors.secondaryColor,
+            fontSize: ManagerFontSize.s26,
+            fontWeight: ManagerFontWeight.bold,
+            letterSpacing: ManagerConestant.letterSpacing3),
+      ),
+      SizedBox(
+        height: ManagerHeight.h70,
+        child: Text(
+          subtitle,
+          style: TextStyle(
+            color: ManagerColors.secondaryColor,
+            fontSize: ManagerFontSize.s16,
+            fontWeight: ManagerFontWeight.Regular,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      const SizedBox(
+        height: ManagerHeight.h20,
+      )
+    ],
+  );
+}
