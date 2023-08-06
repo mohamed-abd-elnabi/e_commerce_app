9c06d797b6aa485174a05f70dddb7170721696d1
Your Name <you@example.com>
2023-08-06 03:40:43 +0300

    add Strings, raduis, and sizes

--- lib/core/resources/manager_strings.dart
+++ lib/core/resources/manager_strings.dart
@@ -1,3 +1,14 @@
 class ManagerStrings {
-  static const String appName = 'أذكار';
+  static const String outboardingTitleOne = 'Title One';
+  static const String outboardingTitleTwo = 'Title Two';
+  static const String outboardingTitleThree = 'Title Three';
+  static const String outboardingTitleFour = 'Title Four';
+  static const String subtitle1 =
+      'Quia sed consequatur rem quia molestias nulla quia. Eos optio'
+      ' soluta asperiores in similique. Eius tenetur blanditiis ad cumque '
+      'aspernatur earum nihil qui. Eveniet doloribus cum eum rerum facilis '
+      'aut. Et quos dolores eos sed ex voluptatem dolorem est ut. Alias voluptatum'
+      ' dolorem occaecati cum atque in dolorum sequi qui.';
+  static const String start = 'Start';
+  static const String naxt = 'Next';
 }
