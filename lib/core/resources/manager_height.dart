d2b5fea0697acf243afc5d0aeff199cac192494b
Your Name <you@example.com>
2023-08-06 03:40:07 +0300

    add Strings, colors, constant, assets and sizes

--- lib/core/resources/manager_height.dart
+++ lib/core/resources/manager_height.dart
@@ -1,8 +1,12 @@
 class ManagerHeight {
   static const double h50 = 50;
+  static const double h20 = 20;
+  static const double h34 = 34;
   static const double h60 = 60;
   static const double h70 = 70;
   static const double h80 = 80;
+  static const double h8 = 8;
+  static const double h9 = 9;
   static const double h90 = 90;
   static const double h100 = 100;
   static const double h200 = 200;
