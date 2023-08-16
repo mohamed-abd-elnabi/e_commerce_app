2dcffa5f506e0cc5256e7e9df37e7f9fd5535712
Your Name <you@example.com>
2023-08-16 11:20:30 +0300

    add routs

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -2,4 +2,5 @@ class Routes {
   static const String splachScreen = '/splach_screen';
   static const String outBoarding = '/out_boarding';
   static const String mainScreen = '/main_screen';
+  static const String authScreen = '/authentication_screen';
 }
