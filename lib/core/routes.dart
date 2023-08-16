d9253ea65ecbfcd5241f9127181ce0e752baa89b
Your Name <you@example.com>
2023-08-16 18:44:06 +0300

    add Authentication routes

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -3,4 +3,6 @@ class Routes {
   static const String outBoarding = '/out_boarding';
   static const String mainScreen = '/main_screen';
   static const String authScreen = '/authentication_screen';
+  static const String loginView = '/loginview_screen';
+  static const String registerView = '/register_screen';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:shop_avatar/core/routes.dart';
+import 'package:shop_avatar/features/auth/presentation/view/register.dart';
 import 'package:shop_avatar/features/out_boarding/presentation/view/out_boarding_screen.dart';
 import 'package:shop_avatar/features/splach/presentation/view/splach_screen.dart';
 
 import 'features/auth/presentation/view/authentication.dart';
+import 'features/auth/presentation/view/login_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,6 +26,8 @@ class MyApp extends StatelessWidget {
         Routes.splachScreen: (context) => const SplachScreen(),
         Routes.outBoarding: (context) => const OutBoarding(),
         Routes.authScreen: (context) => const Authentication(),
+        Routes.loginView: (context) => const LoginView(),
+        Routes.registerView: (context) => const RegisterView(),
       },
     );
   }
