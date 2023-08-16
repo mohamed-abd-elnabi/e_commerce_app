5cacccd5ca1138e0e9b3af99b4b3983be5a70c7b
Your Name <you@example.com>
2023-08-16 11:19:22 +0300

    add constants

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -2,6 +2,12 @@ class ManagerConestant {
   static const int SplachTime = 4;
   static const double elevationAppbar = 0;
   static const double letterSpacing3 = 3;
+  static const int LastPageindex = 3;
   static const double paddingpage = 28;
   static const int pageViewSliderDuration = 600;
+  static const int Durationoutbording = 5000;
+  static const double letterSp5 = 5;
+  static const double OpacityBgauthscreen = 0.7;
+  static const double OpacityButtonauthscreen = 0.6;
+  static const int Spacer3 = 3;
 }
