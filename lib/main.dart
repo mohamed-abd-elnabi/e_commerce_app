1e1d94c0944a9d8bb8a84261df8aaa8cba98d019
Your Name <you@example.com>
2023-08-19 02:09:31 +0300

    edit routes main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shop_avatar/core/routes.dart';
-import 'package:shop_avatar/features/auth/presentation/view/register.dart';
-import 'package:shop_avatar/features/out_boarding/presentation/view/out_boarding_screen.dart';
-import 'package:shop_avatar/features/splach/presentation/view/splach_screen.dart';
-
-import 'features/auth/presentation/view/authentication.dart';
-import 'features/auth/presentation/view/login_view.dart';
+import 'package:get/get.dart';
+import 'package:shop_avatar/routes/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,19 +11,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
       ),
       initialRoute: Routes.splachScreen,
-      routes: {
-        Routes.splachScreen: (context) => const SplachScreen(),
-        Routes.outBoarding: (context) => const OutBoarding(),
-        Routes.authScreen: (context) => const Authentication(),
-        Routes.loginView: (context) => const LoginView(),
-        Routes.registerView: (context) => const RegisterView(),
-      },
+      onGenerateRoute: RouteGenerator.getRoute,
+      //   routes: {
+      //     Routes.splachScreen: (context) => const SplachScreen(),
+      //     Routes.outBoarding: (context) => const OutBoarding(),
+      //     Routes.authScreen: (context) => const Authentication(),
+      //     Routes.loginView: (context) => const LoginView(),
+      //     Routes.registerView: (context) => const RegisterView(),
+      //   },
     );
   }
 }
