11a201430bb2a392531ef4c8c09b5aab1ccae9c5
Your Name <you@example.com>
2023-08-20 02:11:08 +0300

    create  initModule fun

---
+++ lib/config/localization/dependancy_injection.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'package:shop_avatar/core/storage/local/database/shared_preferences/app_setings_shared_preferences.dart';
+
+initModule() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppSittingsSharedPreferences().initPreferences();
+}
