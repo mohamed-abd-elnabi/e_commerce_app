8a9a8ea8d15bd2795cb459e6a11571fcb29805bc
Your Name <you@example.com>
2023-08-20 12:00:30 +0300

    put translations in ManagerStrings

--- lib/core/resources/manager_strings.dart
+++ lib/core/resources/manager_strings.dart
@@ -1,32 +1,35 @@
+import 'package:easy_localization/easy_localization.dart';
+
 class ManagerStrings {
-  static const String outboardingTitleOne = 'Title One';
-  static const String outboardingTitleTwo = 'Title Two';
-  static const String outboardingTitleThree = 'Title Three';
-  static const String outboardingTitleFour = 'Title Four';
-  static const String subtitle1 =
+  static String get outboardingTitleOne => tr('Title One');
+  static String get outboardingTitleTwo => tr('Title Two');
+  static String get outboardingTitleThree => tr('Title Three');
+  static String get outboardingTitleFour => tr('Title Four');
+  static String get subtitle1 => tr(
       'Quia sed consequatur rem quia molestias nulla quia. Eos optio'
       ' soluta asperiores in similique. Eius tenetur blanditiis ad cumque '
       'aspernatur earum nihil qui. Eveniet doloribus cum eum rerum facilis '
       'aut. Et quos dolores eos sed ex voluptatem dolorem est ut. Alias voluptatum'
-      ' dolorem occaecati cum atque in dolorum sequi qui.';
-  static const String start = 'Start';
-  static const String naxt = 'Next';
-  static const String skip = 'Skip';
-  static const String welcome = 'WelCOME to';
-  static const String new4shop = 'new4shop';
-  static const String signUp = 'Sign Up';
-  static const String signIn = 'Sign In';
-  static const String visitor = 'Visitor';
-  static const String login = 'Login';
-  static const String email = 'Email';
-  static const String password = 'Password';
-  static const String rememberMe = '  Remember me';
-  static const String forgetPassword = 'Forgot your password?';
-  static const String haveAcoount = 'Don\'t have an account?';
-  static const String or = 'OR';
-  static const String username = 'Username';
-  static const String numberCity = '🇵🇸    Ex : 986-786-421';
-  static const String confirmPassword = 'Confirm Password';
-  static const String alreadyhaveAcoount = 'Already have account?';
-  static const String notFoundRoute = 'notFoundRoute';
+      ' dolorem occaecati cum atque in dolorum sequi qui.');
+  static String get start => tr('Start');
+  static String get naxt => tr('Next');
+  static String get skip => tr('Skip');
+  static String get welcome => tr('WelCOME to');
+  static String get new4shop => tr('new4shop');
+  static String get signUp => tr('Sign Up');
+  static String get signIn => tr('Sign In');
+  static String get visitor => tr('Visitor');
+  static String get login => tr('Login');
+  static String get email => tr('Email');
+  static String get password => tr('Password');
+  static String get rememberMe => tr('  Remember me');
+  static String get forgetPassword => tr('Forgot your password?');
+  static String get haveAcoount => tr('Don\'t have an account?');
+  static String get or => tr('OR');
+  static String get username => tr('Username');
+  static String get numberCity => tr('🇵🇸    Ex : 986-786-421');
+  static String get confirmPassword => tr('Confirm Password');
+  static String get alreadyhaveAcoount => tr('Already have account?');
+  static String get notFoundRoute => tr('notFoundRoute');
+  static String get mohammed => tr('Mohammed');
 }
