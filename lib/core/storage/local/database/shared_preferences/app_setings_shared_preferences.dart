17b59eacba6139d178b8fd11401c607c5dea3a8e
Your Name <you@example.com>
2023-08-20 11:57:11 +0300

    put locale functions in AppSettingsSharedPreferences

--- lib/core/storage/local/database/shared_preferences/app_setings_shared_preferences.dart
+++ lib/core/storage/local/database/shared_preferences/app_setings_shared_preferences.dart
@@ -2,22 +2,33 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shop_avatar/core/constants.dart';
 import 'package:shop_avatar/core/extinsion/extinsion.dart';
 
-class AppSittingsSharedPreferences {
-  static final _instanse = AppSittingsSharedPreferences._internul();
+class AppSettingsSharedPreferences {
+  static final _instance = AppSettingsSharedPreferences._internal();
   late SharedPreferences _sharedPreferences;
-  AppSittingsSharedPreferences._internul();
 
-  factory AppSittingsSharedPreferences() {
-    return _instanse;
+  AppSettingsSharedPreferences._internal();
+
+  factory AppSettingsSharedPreferences() {
+    return _instance;
   }
+
   Future<void> initPreferences() async {
     _sharedPreferences = await SharedPreferences.getInstance();
   }
 
+//////
   Future<void> saveViewedOutBoarding() async {
     await _sharedPreferences.setBool(keyConestant.outBoardingViewedKey, true);
   }
 
   bool get outBoardingViewed =>
       _sharedPreferences.getBool(keyConestant.outBoardingViewedKey).onNull();
+
+//////
+  Future<void> setDefaultLocale(String lang) async {
+    await _sharedPreferences.setString(keyConestant.localeKey, lang);
+  }
+
+  String get defaultLocale =>
+      _sharedPreferences.getString(keyConestant.localeKey).parseLocale();
 }
