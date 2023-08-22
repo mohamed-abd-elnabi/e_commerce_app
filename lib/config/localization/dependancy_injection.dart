a74a8c98ef9309123e9254bf1fafacf8ea9f70d2
Your Name <you@example.com>
2023-08-22 19:08:20 +0300

    create dependancy injection functions for authentication featchers

--- lib/config/localization/dependancy_injection.dart
+++ lib/config/localization/dependancy_injection.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 import 'package:shop_avatar/core/storage/local/database/shared_preferences/app_setings_shared_preferences.dart';
+
+import '../../features/auth/presentation/controller/auth_controller.dart';
 
 initModule() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppSettingsSharedPreferences().initPreferences();
+}
+
+initAuth() async {
+  Get.put<AuthController>(AuthController());
+}
+
+disposeAuth() async {
+  Get.delete<AuthController>();
 }
