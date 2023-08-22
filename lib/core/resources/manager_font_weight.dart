ae92c57afac5dcbe0aa57c7c2ed7668cf6d1852e
Your Name <you@example.com>
2023-08-22 15:58:01 +0300

    add FontWeight

--- lib/core/resources/manager_font_weight.dart
+++ lib/core/resources/manager_font_weight.dart
@@ -1,10 +1,11 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 class ManagerFontWeight {
   static const FontWeight w600 = FontWeight.w600;
   static const FontWeight bold = FontWeight.w700;
   static const FontWeight w800 = FontWeight.w800;
-  static const FontWeight Regular = FontWeight.w500;
+  static const FontWeight regular = FontWeight.w500;
   static const FontWeight w300 = FontWeight.w300;
   static const FontWeight w400 = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
 }
