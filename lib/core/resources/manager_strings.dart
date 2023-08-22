416e99d651ef08d056cee1ee9a822cca0252e5d6
Your Name <you@example.com>
2023-08-22 03:42:55 +0300

    fix bugs in ManagerStrings

--- lib/core/resources/manager_strings.dart
+++ lib/core/resources/manager_strings.dart
@@ -1,35 +1,30 @@
 import 'package:easy_localization/easy_localization.dart';
 
 class ManagerStrings {
-  static String get outboardingTitleOne => tr('Title One');
-  static String get outboardingTitleTwo => tr('Title Two');
-  static String get outboardingTitleThree => tr('Title Three');
-  static String get outboardingTitleFour => tr('Title Four');
-  static String get subtitle1 => tr(
-      'Quia sed consequatur rem quia molestias nulla quia. Eos optio'
-      ' soluta asperiores in similique. Eius tenetur blanditiis ad cumque '
-      'aspernatur earum nihil qui. Eveniet doloribus cum eum rerum facilis '
-      'aut. Et quos dolores eos sed ex voluptatem dolorem est ut. Alias voluptatum'
-      ' dolorem occaecati cum atque in dolorum sequi qui.');
-  static String get start => tr('Start');
-  static String get naxt => tr('Next');
-  static String get skip => tr('Skip');
-  static String get welcome => tr('WelCOME to');
+  static String get outboardingTitleOne => tr('outboardingTitleOne');
+  static String get outboardingTitleTwo => tr('outboardingTitleTwo');
+  static String get outboardingTitleThree => tr('outboardingTitleThree');
+  static String get outboardingTitleFour => tr('outboardingTitleFour');
+  static String get subtitle1 => tr('subtitle1');
+  static String get start => tr('start');
+  static String get naxt => tr('naxt');
+  static String get skip => tr('skip');
+  static String get welcome => tr('welcome');
   static String get new4shop => tr('new4shop');
-  static String get signUp => tr('Sign Up');
-  static String get signIn => tr('Sign In');
-  static String get visitor => tr('Visitor');
-  static String get login => tr('Login');
-  static String get email => tr('Email');
-  static String get password => tr('Password');
-  static String get rememberMe => tr('  Remember me');
-  static String get forgetPassword => tr('Forgot your password?');
-  static String get haveAcoount => tr('Don\'t have an account?');
-  static String get or => tr('OR');
-  static String get username => tr('Username');
-  static String get numberCity => tr('🇵🇸    Ex : 986-786-421');
-  static String get confirmPassword => tr('Confirm Password');
-  static String get alreadyhaveAcoount => tr('Already have account?');
+  static String get signUp => tr('signUp');
+  static String get signIn => tr('signIn');
+  static String get visitor => tr('visitor');
+  static String get login => tr('login');
+  static String get email => tr('email');
+  static String get password => tr('password');
+  static String get rememberMe => tr('rememberMe');
+  static String get forgetPassword => tr('forgetPassword');
+  static String get haveAcoount => tr('haveAcoount');
+  static String get or => tr('or');
+  static String get username => tr('username');
+  static String get numberCity => tr('numberCity');
+  static String get confirmPassword => tr('confirmPassword');
+  static String get alreadyhaveAcoount => tr('alreadyhaveAcoount');
   static String get notFoundRoute => tr('notFoundRoute');
-  static String get mohammed => tr('Mohammed');
+  static String get mohammed => tr('mohammed');
 }
