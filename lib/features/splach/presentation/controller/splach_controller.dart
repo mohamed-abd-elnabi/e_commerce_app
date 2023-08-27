7922a5c362d6e0390a2d17e9d117f2d1652da108
Your Name <you@example.com>
2023-08-27 17:52:22 +0300

    create SplashController

---
+++ lib/features/splach/presentation/controller/splach_controller.dart
@@ -0,0 +1,24 @@
+import 'package:get/get.dart';
+import '../../../../core/constants.dart';
+import '../../../../core/storage/local/database/shared_preferences/app_setings_shared_preferences.dart';
+import '../../../../routes/routes.dart';
+
+class SplashController extends GetxController {
+  AppSettingsSharedPreferences appSettingsSharedPreferences =
+      AppSettingsSharedPreferences();
+  //BuildContext? context;
+  @override
+  void onInit() {
+    super.onInit();
+    //appSettingsSharedPreferences.clear();
+    Future.delayed(Duration(seconds: ManagerConestant.SplachTime), () {
+      print(appSettingsSharedPreferences.loggedIn);
+      String route = appSettingsSharedPreferences.loggedIn
+          ? Routes.homePage
+          : appSettingsSharedPreferences.outBoardingViewed
+              ? Routes.authScreen
+              : Routes.outBoarding;
+      Get.offAllNamed(route);
+    });
+  }
+}
