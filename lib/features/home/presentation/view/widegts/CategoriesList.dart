f564b1ddea7728e1b8350250c23ed20093c48a42
Your Name <you@example.com>
2023-09-02 15:08:27 +0300

    create CategoriesList

---
+++ lib/features/home/presentation/view/widegts/CategoriesList.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../../../../../core/resources/manager_colors.dart';
+import '../../../../../core/resources/manager_height.dart';
+import '../../../../../core/resources/manager_raduis.dart';
+import '../../../../../core/resources/manager_weight.dart';
+import '../../controller/home_controller..dart';
+
+SizedBox CategoriesList(HomeController controller) {
+  return SizedBox(
+    height: ManagerHeight.h100,
+    child: ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: controller.categoryList.length,
+      itemBuilder: (context, index) {
+        return Container(
+          height: ManagerHeight.h100,
+          width: ManagerWeight.w100,
+          margin: EdgeInsetsDirectional.only(
+            end: ManagerWeight.w8,
+          ),
+          decoration: BoxDecoration(
+            color: ManagerColors.white,
+            borderRadius: BorderRadius.circular(ManagerRaduis.r12),
+          ),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.network(
+                    height: ManagerHeight.h50,
+                    controller.categoryList[index].banner),
+                Text(
+                  maxLines: 1,
+                  controller.categoryList[index].name,
+                  style: const TextStyle(overflow: TextOverflow.ellipsis),
+                ),
+              ]),
+        );
+      },
+    ),
+  );
+}
