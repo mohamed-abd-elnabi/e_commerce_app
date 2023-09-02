ed74d744ce20dd1a608addefb9d3e1a09aa497d2
Your Name <you@example.com>
2023-09-02 15:09:50 +0300

    create home request

--- lib/features/home/data/data_source/home_api_controller.dart
+++ lib/features/home/data/data_source/home_api_controller.dart
@@ -4,8 +4,11 @@ import 'package:http/http.dart' as http;
 import 'package:shop_avatar/core/storage/local/database/shared_preferences/app_setings_shared_preferences.dart';
 import 'package:shop_avatar/core/widegts/helpers.dart';
 import 'package:shop_avatar/features/home/data/mapper/category_mapper.dart';
+import 'package:shop_avatar/features/home/data/mapper/home_mapper.dart';
 import 'package:shop_avatar/features/home/data/response/category_response.dart';
+import 'package:shop_avatar/features/home/data/response/home_response.dart';
 import 'package:shop_avatar/features/home/presentation/model/category_model.dart';
+import 'package:shop_avatar/features/home/presentation/model/home_model.dart';
 import '../../../../core/constants.dart';
 
 class HomeApiController with Helpers {
@@ -20,6 +23,26 @@ class HomeApiController with Helpers {
       Uri.parse(endPoint),
       headers: headers,
     );
+  }
+
+  Future<HomeModel> home({required BuildContext context}) async {
+    http.Response response = await get(endPoint: ApiRequest.home, headers: {});
+    var statusCode = response.statusCode;
+    if (statusCode >= 200 && statusCode < 300) {
+      var json = jsonDecode(response.body);
+      return HomeResponse.fromJson(json).toDomain();
+    } else if (statusCode >= 400 && statusCode > 400) {
+      var json = jsonDecode(response.body);
+      return HomeModel(
+          data: [], success: json['success'], status: json['status']);
+    }
+
+    showSnackBar(
+      context: context,
+      message: 'Somthing Went Erorr',
+      error: true,
+    );
+    return HomeModel(data: [], success: false, status: 404);
   }
 
   Future<List<CategoryModel>> categories(
