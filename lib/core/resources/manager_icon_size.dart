a94c14e704db11c534b4b5ef6a1895560f6886b1
Your Name <you@example.com>
2023-09-04 11:50:15 +0300

    add managers

--- lib/core/resources/manager_icon_size.dart
+++ lib/core/resources/manager_icon_size.dart
@@ -5,6 +5,7 @@ class ManagerIconSize {
   static const double s20 = 20;
   static const double s24 = 24;
   static const double s26 = 26;
+  static const double s34 = 34;
   static const double s36 = 36;
   static const double s30 = 30;
 }
