6fc6fb6f60f0e41a2a7bfa08875b445b59a38157
Your Name <you@example.com>
2023-09-14 18:25:09 +0300

    create ProductDetailsBrandModel

---
+++ lib/features/product_details/domain/model/product_details_brand_model.dart
@@ -0,0 +1,11 @@
+class ProductDetailsBrandModel {
+  int id;
+  String name;
+  String logo;
+
+  ProductDetailsBrandModel({
+    required this.id,
+    required this.name,
+    required this.logo,
+  });
+}
