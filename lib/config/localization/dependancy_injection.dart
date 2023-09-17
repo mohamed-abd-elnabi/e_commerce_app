e54fd26be4d6c0a5ae2552b6faf213262c4dd966
Your Name <you@example.com>
2023-09-17 16:45:11 +0300

    create initProductDetails

--- lib/config/localization/dependancy_injection.dart
+++ lib/config/localization/dependancy_injection.dart
@@ -1,36 +1,54 @@
+import '../../core/storage/local/database/shared_preferences/app_setings_shared_preferences.dart';
+import '../../features/auth/presentation/controller/auth_controller.dart';
+import '../../features/product_details/domain/repository/product_details_repository.dart';
+import '../../features/splach/presentation/controller/splach_controller.dart';
+import '/core/internet_checker/internet_checker.dart';
+import '/core/network/api/app_api.dart';
+import '/core/network/api/dio_factory.dart';
+import '/features/home/presentation/controller/home_controller.dart';
+import '/features/product_details/data/data_source/remote_data_source.dart';
+import '/features/product_details/domain/use_case/product_details_use_case.dart';
 import 'package:dio/dio.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
-import 'package:shop_avatar/core/internet_checker/internet_checker.dart';
-import 'package:shop_avatar/core/network/api/app_api.dart';
-import 'package:shop_avatar/core/network/api/dio_factory.dart';
-import 'package:shop_avatar/core/storage/local/database/shared_preferences/app_setings_shared_preferences.dart';
-import 'package:shop_avatar/features/home/presentation/controller/home_controller.dart';
-import '../../features/auth/presentation/controller/auth_controller.dart';
-import '../../features/product_details/presentation/controller/product_details_controller.dart';
-import '../../features/splach/presentation/controller/splach_controller.dart';
 
 final instance = GetIt.instance;
 
 initModule() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await AppSettingsSharedPreferences().initPreferences();
 
   if (!GetIt.I.isRegistered<NetworkInfo>()) {
     instance.registerLazySingleton<NetworkInfo>(
-        () => NetworkInfoImplementation(InternetConnectionCheckerPlus()));
+      () => NetworkInfoImplementation(
+        InternetConnectionCheckerPlus(),
+      ),
+    );
   }
 
   if (!GetIt.I.isRegistered<DioFactory>()) {
-    instance.registerLazySingleton<DioFactory>(() => DioFactory());
+    instance.registerLazySingleton<DioFactory>(
+      () => DioFactory(),
+    );
   }
 
   if (!GetIt.I.isRegistered<AppApi>()) {
     Dio dio = await instance<DioFactory>().getDio();
-    instance.registerLazySingleton<AppApi>(() => AppApi(dio));
+    instance.registerLazySingleton<AppApi>(
+      () => AppApi(dio),
+    );
   }
+}
+
+initSplash() {
+  Get.put<SplashController>(SplashController());
+}
+
+disposeSplash() {
+  Get.delete<SplashController>();
 }
 
 initAuth() {
@@ -42,27 +60,41 @@ disposeAuth() {
   Get.delete<AuthController>();
 }
 
-initSplash() {
-  Get.put<SplashController>(SplashController());
-}
-
-disposeSplash() {
-  Get.delete<SplashController>();
-}
-
 initHome() {
   disposeSplash();
   disposeAuth();
-  initDetails();
+  initProductDetails();
+
   Get.put<HomeController>(HomeController());
 }
 
 disposeHome() {}
 
-initDetails() {
-  Get.put<ProductDetailsController>(ProductDetailsController());
+initProductDetails() {
+  if (!GetIt.I.isRegistered<ProductDetailsRemoteDataSource>()) {
+    instance.registerLazySingleton<ProductDetailsRemoteDataSource>(
+      () => ProductDetailsDataSourceImplementation(
+        instance<AppApi>(),
+      ),
+    );
+  }
+
+  if (!GetIt.I.isRegistered<ProductDetailsRepository>()) {
+    instance.registerLazySingleton<ProductDetailsRepository>(
+      () => ProductDetailsRepositoryImplementation(
+        data: instance(),
+        networkInfo: instance(),
+      ),
+    );
+  }
+
+  if (!GetIt.I.isRegistered<ProductDetailsUseCaseImplementation>()) {
+    instance.registerLazySingleton<ProductDetailsUseCaseImplementation>(
+      () => ProductDetailsUseCaseImplementation(
+        instance(),
+      ),
+    );
+  }
 }
 
-disposeDetails() {
-  Get.delete<ProductDetailsController>();
-}
+disposeProductDetails() {}
