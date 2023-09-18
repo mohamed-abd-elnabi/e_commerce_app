b5cd3ce2e0f0c6e8b1475af691840f4bebd372df
Your Name <you@example.com>
2023-09-18 18:36:10 +0300

    create POST requset forgetPassword

--- lib/core/network/api/app_api.dart
+++ lib/core/network/api/app_api.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import '../../../features/forget_password/data/response/forget_password_response.dart';
 import '../../../features/product_details/data/respose/product_details_response.dart';
 import '../../constants.dart';
 import 'package:retrofit/retrofit.dart';
@@ -10,4 +11,9 @@ abstract class AppApi {
 
   @GET(ApiRequest.productDetails)
   Future<ProductDetailsResponse> productDetails(@Path(ApiConstants.id) int id);
+
+  @POST(ApiRequest.forgetPasswordRequest)
+  Future<ForgetPasswordResponse> forgetPassword(
+    @Field(ApiConstants.email) String email,
+  );
 }
