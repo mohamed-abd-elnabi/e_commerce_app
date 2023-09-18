62cd2a1873f93a5383cdf956e27c09044bf09ef7
Your Name <you@example.com>
2023-09-18 18:36:52 +0300

    create CodeVerification

---
+++ lib/features/forget_password/presentation/view/widget/code_verification.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:shop_avatar/core/resources/manager_raduis.dart';
+import 'package:shop_avatar/core/resources/manager_weight.dart';
+
+import '../../../../../core/resources/manager_colors.dart';
+
+class CodeVerification extends StatelessWidget {
+  final TextEditingController controller;
+  final FocusNode focusNode;
+  final FocusNode previousFocusNode;
+  final void Function(String value) onChanged;
+  final String? Function(String?)? validator;
+
+  const CodeVerification({
+    Key? key,
+    required this.controller,
+    required this.focusNode,
+    required this.previousFocusNode,
+    required this.onChanged,
+    required this.validator,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextFormField(
+        controller: controller,
+        focusNode: focusNode,
+        validator: validator,
+        keyboardType: TextInputType.number,
+        maxLength: 1,
+        textInputAction: TextInputAction.done,
+        cursorColor: ManagerColors.primaryColor,
+        onChanged: (value) {
+          onChanged(value);
+          if (value.isEmpty) {
+            previousFocusNode.requestFocus();
+          }
+        },
+        textAlign: TextAlign.center,
+        decoration: InputDecoration(
+            filled: true,
+            fillColor: ManagerColors.homeScaffoldBackGround,
+            counterText: '',
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(
+                ManagerRaduis.r10,
+              ),
+              borderSide: BorderSide(
+                width: ManagerWeight.w2,
+                color: ManagerColors.primaryColor,
+              ),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(
+                ManagerRaduis.r10,
+              ),
+              borderSide: BorderSide(
+                width: ManagerWeight.w2,
+                color: Colors.white,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+                borderSide:
+                    BorderSide(color: ManagerColors.homeScaffoldBackGround))),
+      ),
+    );
+  }
+}
