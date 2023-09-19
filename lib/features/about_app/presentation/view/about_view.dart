485098557f08be945a2dcffbd1c756ceddb2c079
Your Name <you@example.com>
2023-09-19 13:37:18 +0300

    refactor AboutView

--- lib/features/about_app/presentation/view/about_view.dart
+++ lib/features/about_app/presentation/view/about_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop_avatar/core/resources/manager_colors.dart';
 import 'package:shop_avatar/core/resources/manager_height.dart';
-import 'package:shop_avatar/core/resources/manager_text_styles.dart';
+import 'package:shop_avatar/core/resources/manager_text_styles_light.dart';
 
 class AboutView extends StatelessWidget {
   const AboutView({super.key});
