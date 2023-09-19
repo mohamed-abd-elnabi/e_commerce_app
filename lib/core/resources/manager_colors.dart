0bc9d56f7cc64bf05ea2f34114f76acc0cd79644
Your Name <you@example.com>
2023-09-19 13:58:38 +0300

    add color

--- lib/core/resources/manager_colors.dart
+++ lib/core/resources/manager_colors.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
 class ManagerColors {
+  /////////////////////////////////////////////////
+  //ManagerColorsLight
   static const Color blake = Colors.black;
   static const Color white = Colors.white;
   static Color grey = Colors.grey.shade700;
   static Color greyLight = Colors.grey.shade300;
   static const Color primaryColor = Color(0xffFF7F3F);
   static const Color primaryTextColor = Color(0xff393939);
+  static const Color primaryTextColorDark = Color(0xffded9d9);
   static Color secondaryColor = Color(0xff393939);
   static Color prossColor = Color(0xff979797);
   static Color error = Colors.red;
   static Color transparent = Colors.transparent;
   static const Color homeScaffoldBackGround = Color(0xffF8F8FC);
+
+/////////////////////////////////////////////////
+//ManagerColorsDark
 }
