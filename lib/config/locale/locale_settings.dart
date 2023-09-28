01447e6189903a0bf42f5b4ef31c8ef5e5b191c9
Your Name <you@example.com>
2023-09-28 12:56:48 +0300

    refactor LocaleSettings

--- lib/config/locale/locale_settings.dart
+++ lib/config/locale/locale_settings.dart
@@ -5,15 +5,17 @@ import 'package:shop_avatar/core/storage/local/database/shared_preferences/app_s
 LocaleSettings localeSettings = LocaleSettings();
 
 class LocaleSettings {
-  final AppSettingsSharedPreferences _appSittingsSharedPreferences =
+  final AppSettingsSharedPreferences _appSettingsSharedPreferences =
       AppSettingsSharedPreferences();
+
   static Map<String, String> languages = {
     keyConestant.arabic: keyConestant.arabicName,
     keyConestant.english: keyConestant.englishName,
   };
+
   List<Locale> get locales =>
       languages.keys.map((langKey) => Locale(langKey)).toList();
 
   Locale get defaultLocale =>
-      Locale(_appSittingsSharedPreferences.defaultLocale);
+      Locale(_appSettingsSharedPreferences.defaultLocale);
 }
