54bd35c09713677a41b8747ed628fbf463c5f787
Your Name <you@example.com>
2023-09-28 12:58:52 +0300

    create EditProfileView

--- lib/features/profile/presentation/view/edir_profile_view.dart
+++ lib/features/profile/presentation/view/edir_profile_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/simple/get_state.dart';
 import 'package:shop_avatar/core/resources/manager_assets.dart';
 import 'package:shop_avatar/core/resources/manager_colors.dart';
 import 'package:shop_avatar/core/resources/manager_icon_size.dart';
 import 'package:shop_avatar/core/resources/manager_raduis.dart';
 import 'package:shop_avatar/core/resources/manager_weight.dart';
+import 'package:shop_avatar/features/profile/presentation/controller/profile_controller.dart';
 import '../../../../core/resources/managar_font_size.dart';
 import '../../../../core/resources/manager_height.dart';
 import '../../../../core/resources/manager_spacer.dart';
@@ -16,119 +18,133 @@ class EditProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          actions: [
-            const Icon(Icons.check),
-            SizedBox(
-              width: ManagerWeight.w16,
-            )
-          ],
-          title: Text(ManagerStrings.editProfile.toUpperCase(),
-              style: getRegularTextStyle(fontSize: ManagerFontSize.s18)),
-          centerTitle: true,
-          backgroundColor: ManagerColors.homeScaffoldBackGround,
-        ),
-        body: Container(
-          margin:
-              EdgeInsetsDirectional.symmetric(horizontal: ManagerWeight.w26),
-          child: Column(
-            children: [
+    return GetBuilder<ProfileController>(builder: (controller) {
+      return Scaffold(
+          appBar: AppBar(
+            actions: [
+              const Icon(Icons.check),
               SizedBox(
-                height: ManagerHeight.h26,
-              ),
-              Stack(
-                alignment: AlignmentDirectional.center,
+                width: ManagerWeight.w16,
+              )
+            ],
+            title: Text(ManagerStrings.editProfile.toUpperCase(),
+                style: getRegularTextStyle(fontSize: ManagerFontSize.s18)),
+            centerTitle: true,
+            backgroundColor: ManagerColors.homeScaffoldBackGround,
+          ),
+          body: SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsetsDirectional.symmetric(
+                  horizontal: ManagerWeight.w26),
+              child: Column(
                 children: [
-                  Image.asset(ManagerAssets.profile),
-                  Container(
-                    height: ManagerHeight.h120,
-                    width: ManagerWeight.w120,
-                    decoration: BoxDecoration(
-                        color: Colors.black.withOpacity(0.6),
-                        borderRadius: BorderRadiusDirectional.circular(
-                            ManagerRaduis.r100)),
+                  SizedBox(
+                    height: ManagerHeight.h26,
                   ),
-                  const Icon(
-                    Icons.camera_alt_outlined,
-                    size: ManagerIconSize.s36,
-                    color: ManagerColors.white,
-                  )
+                  Stack(
+                    alignment: AlignmentDirectional.center,
+                    children: [
+                      Image.asset(ManagerAssets.profile),
+                      Container(
+                        height: ManagerHeight.h120,
+                        width: ManagerWeight.w120,
+                        decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.6),
+                            borderRadius: BorderRadiusDirectional.circular(
+                                ManagerRaduis.r100)),
+                      ),
+                      const Icon(
+                        Icons.camera_alt_outlined,
+                        size: ManagerIconSize.s36,
+                        color: ManagerColors.white,
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: ManagerHeight.h40,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        prefixText: controller.appSettingsSharedPreferences
+                            .getUserInfo()
+                            .name,
+                        label: Text(
+                          ManagerStrings.userName,
+                          style: getRegularTextStyle(
+                              fontSize: ManagerFontSize.s16,
+                              color: ManagerColors.grey),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: ManagerColors.greyLight)),
+                        focusedBorder: const UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: ManagerColors.primaryColor)),
+                        errorBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: ManagerColors.error))),
+                  ),
+                  SizedBox(
+                    height: ManagerHeight.h20,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        fillColor: Colors.amber,
+                        label: Text(
+                          ManagerStrings.email,
+                          style: getRegularTextStyle(
+                              fontSize: ManagerFontSize.s16,
+                              color: ManagerColors.grey),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: ManagerColors.greyLight)),
+                        focusedBorder: const UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: ManagerColors.primaryColor)),
+                        errorBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: ManagerColors.error))),
+                  ),
+                  SizedBox(
+                    height: ManagerHeight.h20,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        fillColor: Colors.amber,
+                        label: Text(
+                          ManagerStrings.phone,
+                          style: getRegularTextStyle(
+                              fontSize: ManagerFontSize.s16,
+                              color: ManagerColors.grey),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: ManagerColors.greyLight)),
+                        focusedBorder: const UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: ManagerColors.primaryColor)),
+                        errorBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: ManagerColors.error))),
+                  ),
+                  SizedBox(
+                    height: ManagerHeight.h100,
+                  ),
+                  base_button(
+                    title: ManagerStrings.save,
+                    onPreessed: () {},
+                    isVisibalIcon: false,
+                    spacer: ManagerSpacer.s3,
+                    bgColor: ManagerColors.primaryColor,
+                    textStyle: getRegularTextStyle(
+                        color: ManagerColors.white,
+                        fontSize: ManagerFontSize.s18),
+                  ),
                 ],
               ),
-              SizedBox(
-                height: ManagerHeight.h40,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    label: Text(
-                      ManagerStrings.userName,
-                      style: getRegularTextStyle(
-                          fontSize: ManagerFontSize.s16,
-                          color: ManagerColors.grey),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: ManagerColors.greyLight)),
-                    focusedBorder: const UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: ManagerColors.primaryColor)),
-                    errorBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: ManagerColors.error))),
-              ),
-              SizedBox(
-                height: ManagerHeight.h20,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    fillColor: Colors.amber,
-                    label: Text(
-                      ManagerStrings.email,
-                      style: getRegularTextStyle(
-                          fontSize: ManagerFontSize.s16,
-                          color: ManagerColors.grey),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: ManagerColors.greyLight)),
-                    focusedBorder: const UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: ManagerColors.primaryColor)),
-                    errorBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: ManagerColors.error))),
-              ),
-              SizedBox(
-                height: ManagerHeight.h20,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    fillColor: Colors.amber,
-                    label: Text(
-                      ManagerStrings.phone,
-                      style: getRegularTextStyle(
-                          fontSize: ManagerFontSize.s16,
-                          color: ManagerColors.grey),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: ManagerColors.greyLight)),
-                    focusedBorder: const UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: ManagerColors.primaryColor)),
-                    errorBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: ManagerColors.error))),
-              ),
-              SizedBox(
-                height: ManagerHeight.h100,
-              ),
-              base_button(
-                title: ManagerStrings.save,
-                onPreessed: () {},
-                isVisibalIcon: false,
-                spacer: ManagerSpacer.s3,
-                bgColor: ManagerColors.primaryColor,
-                textStyle: getRegularTextStyle(
-                    color: ManagerColors.white, fontSize: ManagerFontSize.s18),
-              ),
-            ],
-          ),
-        ));
+            ),
+          ));
+    });
   }
 }
